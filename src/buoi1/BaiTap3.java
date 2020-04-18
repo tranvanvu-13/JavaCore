d113e5347d7e84a49355173e33eb94caeb71ae5f
tranvanvu <vudeeptr.359@gmail.com>
2020-04-18 11:00:10 +0700

    bai tap

---
+++ src/buoi1/BaiTap3.java
@@ -0,0 +1,36 @@
+package buoi1;
+
+import java.util.Scanner;
+
+public class BaiTap3 {
+    public static void main(String[] args) {
+        System.out.println("Nhập Vào Số 8");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b , c , d , e, f , g , h , j , k , l ;
+        b = a * 1;
+        c = a * 2;
+        d = a * 3;
+        e = a * 4;
+        f = a * 5;
+        g = a * 6;
+        h = a * 7;
+        j = a * 8;
+        k = a * 9;
+        l = a * 10;
+        System.out.println("Kết Quả : ");
+        System.out.println("8 x 1 = " +b);
+        System.out.println("8 x 2 = " +c);
+        System.out.println("8 x 3 = " +d);
+        System.out.println("8 x 4 = " +e);
+        System.out.println("8 x 5 = " +f);
+        System.out.println("8 x 6 = " +g);
+        System.out.println("8 x 7 = " +h);
+        System.out.println("8 x 8 = " +j);
+        System.out.println("8 x 9 = " +k);
+        System.out.println("8 x 10 = " +l);
+
+
+
+    }
+}
---
+++ src/buoi1/Baitap4.java
@@ -0,0 +1,15 @@
+package buoi1;
+
+import java.util.Scanner;
+
+public class Baitap4 {
+    public static void main(String[] args) {
+        System.out.println(" Nhập Vào 1 Số Bất Kì");
+        Scanner scanner = new Scanner(System.in);
+        int a  = scanner.nextInt();
+        int b;
+        b = a + 55 + 555;
+        System.out.println(+b);
+
+    }
+}
